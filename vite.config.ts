3dfb317d2949e21322a6520841d29aee300f38bd
Desiree Rose <dezrose@me.com>
2025-12-02 18:19:55 -0800

    Update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/sba10-recipe-discoveryapp/", // 👈 repo name goes here
+  base: "https://github.com/dezrose63/ps-sba10-recipe-app", // 👈 repo name goes here
 });
