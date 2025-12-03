9d9f04963b8b907881b7c6a1cdbe1abb53b9349f
dezrose63 <dezrose@me.com>
2025-12-02 21:54:28 -0800

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { FavoritesProvider } from "./context/FavoritesContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter> 
       <FavoritesProvider>
         <App />
       </FavoritesProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
