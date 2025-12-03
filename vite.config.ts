e1fb9d64c5b20e5b17ccafc84e92cefc8dc3324b
dezrose63 <dezrose@me.com>
2025-12-02 23:43:27 -0800

    updated compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/ps-sba10-recipe-app/", // 👈 repo name goes here
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+  ],
 });
